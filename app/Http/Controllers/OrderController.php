e8e2f207cbc6b3d85dc95d128b5d9cd2338af85c
Klincher <camelotvm48@gmail.com>
2022-08-03 11:34:58 +0300

    added updateOrCreate to Client and foreach to counting price sum

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -16,46 +16,17 @@ class OrderController extends Controller
 
     public function store(Request $request)
     {
+        $options = ['area', 'rooms', 'bathrooms', 'kitchens', 'fridges', 'wardrobes', 'animals', 'adults', 'children'];
         $sum = 0;
 
-        if ($request->filled('area')) {
-            $price = Price::where('name', '=', 'area')->first()->price;
-            $sum += (int) $request->area * $price;
-        }
-        if ($request->filled('rooms')) {
-            $price = Price::where('name', '=', 'rooms')->first()->price;
-            $sum += (int) $request->rooms * $price;
-        }
-        if ($request->filled('bathrooms')) {
-            $price = Price::where('name', '=', 'bathrooms')->first()->price;
-            $sum += (int) $request->bathrooms * $price;
-        }
-        if ($request->filled('kitchens')) {
-            $price = Price::where('name', '=', 'kitchens')->first()->price;
-            $sum += (int) $request->kitchens * $price;
-        }
-        if ($request->filled('fridges')) {
-            $price = Price::where('name', '=', 'fridges')->first()->price;
-            $sum += (int) $request->fridges * $price;
-        }
-        if ($request->filled('wardrobes')) {
-            $price = Price::where('name', '=', 'wardrobes')->first()->price;
-            $sum += (int) $request->wardrobes * $price;
-        }
-        if ($request->filled('animals')) {
-            $price = Price::where('name', '=', 'animals')->first()->price;
-            $sum += (int) $request->animals * $price;
-        }
-        if ($request->filled('adults')) {
-            $price = Price::where('name', '=', 'adults')->first()->price;
-            $sum += (int) $request->adults * $price;
-        }
-        if ($request->filled('children')) {
-            $price = Price::where('name', '=', 'children')->first()->price;
-            $sum += (int) $request->children * $price;
+        foreach ($options as $option) {
+            if ($request->filled($option)) {
+                $price = Price::where('name', '=', $option)->first()->price;
+                $sum += (int) $request->input($option) * $price;
+            }
         }
 
-        $client = Client::firstOrCreate(
+        $client = Client::updateOrCreate(
             ['phone' => $request->phone],
             [
                 'first_name' => $request->first_name,
@@ -63,13 +34,6 @@ class OrderController extends Controller
                 'email' => $request->email,
             ]
         );
-
-        // $client = new Client;
-        // $client->phone = $request->phone;
-        // $client->first_name = $request->first_name;
-        // $client->last_name = $request->last_name;
-        // $client->email = $request->email;
-        // $client->save();
 
         $order = new Order;
         $order->client_id = $client->id;
